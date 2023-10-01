7c38022b8e10c5530f274a625a6600fb3ed699c8
AramZap <114266127+AramZap@users.noreply.github.com>
2023-10-01 14:42:27 -0400

    commenting on scripts

--- main.sh
+++ main.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# This script should be run in cron.
+# This script is the main script that should be continuosly run for each IP address.
+# It handles everything, from starting up honeypot configurations on the IP address,
+# recycling a container when the time is up, and running the data collection script
+# at the appropriate times. 
+
+# As for our recycling policy:
+# (1) the maximum amount of time before a honeypot is recycled is 30 minutes from when an
+#     attacker first ssh's into the honeypot. 
+# (2) the amount of idle time before a honeypot is recycled is 5 minutes.
 
 # checking to see if the number of arguments passed in (3) is correct
 if [ $# -ne 3 ]
@@ -17,10 +25,18 @@ container_run_time=$3
 # creates an mitm_logs directory if it doesn't exist yet
 if [ ! -d /home/student/mitm_logs ]
 then
-  mkdir /home/student/mitm_logs
+    mkdir /home/student/mitm_logs
 fi
 
-# case that the tracker document for this ip address already exists, meaning that a container is presently running on the ip address
+# creates an mitm logs directory specifically for the specific ip address, if it doesn't exist yet
+if [ ! -d /home/student/mitm_logs/logs_for_"$ipaddress"]
+then
+    mkdir /home/student/mitm_logs/logs_for_"$ipaddress"
+fi
+
+
+# considers the case that the tracker document for this ip address already exists, 
+# meaning that a container is presently running on the ip address
 if [ -f /home/student/tracker"$ipaddress".txt ]
 then
 
@@ -54,32 +70,44 @@ then
 
   fi
 
-    # exiting from the script
+    # exiting from the script in the case that the container is running but it's not yet time to recycle yet
     exit 0
 fi
 
 # everything below is intended to run if there is no container running on the ip address (so there is no tracker document for it)
 
+# generates a random number between 1-3 (inclusive) to decide on which honeypot configuration will be next to run on this ip address
 randnum=$(openssl rand -hex 1) ; dec=$(printf "%d" "0x$num") ; dec=$(($dec - 2)) ; mod=$(($dec % 3 + 1))
 
-# 
+# if the random number is 1, then honeypot type 1 will run. This is the honeypot with 0% of its files being compressed
 if [ "$randnum" -eq 1 ]
 then
+
     /home/student/container.sh compcontainer "$1" "$2"
     contname="compcontainer"
-
     sudo lxc-attach -n "$contname" -- bash -c "gzip /honey/honeyfile1.txt && gzip /honey/honeyfile2.txt && gzip /honey/honeyfile3.txt"
+
+# if the random number is 2, then honeypot type 2 will run. This is the honeypot with 50% of its files being compressed
 elif [ "$randnum" -eq 2]
 then
     #FIX FOR 50%
     /home/student/container.sh uncompcontainer "$1" "$2"
     contname="uncompcontainer"
+
+# if the random number is 3, then honeypot type 3 will run. This is the honeypot with 100% of its files being compressed
 else
-    
     /home/student/container.sh uncompcontainer "$1" "$2"
     contname="uncompcontainer"
 fi
 
-secondsafterepoch=$(date +%s)
-endsecondsafterepoch=$(("$secondsafterepoch" + 60*"$3"))
-echo "$endsecondsafterepoch" "$contname" "$1" "$2" >> /home/student/tracker.txt
+# current time (using seconds after epoch)
+current_time=$(date +%s)
+
+# maximum amount of time container can run in seconds (30 mins = 1800 secs)
+max_cont_time_in_secs=$(("$container_run_time"*60))
+
+# the end time of the container (using seconds after epoch)
+container_end_time=$(("$current_time" + "$max_cont_time_in_secs"))
+
+# 
+echo "$container_end_time" "$contname" "$1" "$2" >> /home/student/tracker"$ipaddress".txt
