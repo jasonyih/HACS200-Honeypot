de001b41ee0db5bd3a4093e2ed331a81e130bda8
TreyWolf <144627581+TreyWolf@users.noreply.github.com>
2023-10-17 18:16:12 -0400

    Update datacol.sh

--- datacol.sh
+++ datacol.sh
@@ -11,6 +11,7 @@ then
     exit 1
 fi
 
+contname=$1
 # these two variables are used to track the last MITM log file for the given IP address
 fileend=1
 increment=2
@@ -28,8 +29,26 @@ then
     touch /home/student/mitm_logs/"$1"counter
 fi
 
+fileend=$(($fileend- 1))
 # numcommands holds the number of commands that were executed in a particular session
 numcommands=$(grep -c 'line from reader' /home/student/mitm_logs/"$1".log"$fileend")
-
-# numcommands is added to the end of the respective counter file
-echo "$numcommands" >> /home/student/mitm_logs/"$1"counter
+timestamp_of_attacker_entry=$(grep -m 1 'Opened shell' /home/student/mitm_logs/"$contname".log"$fileend" | cut -d' ' -f1-2)
+attacker_entry_in_seconds_after_epoch=$(date -d "$timestamp_of_attacker_entry" +%s)
+timestamp_of_attacker_exit=$(grep -m 1 'Attacker closed the connection' /home/student/mitm_logs/"$contname".log"$fileend" | cut -d' ' -f1-2)
+attacker_exit_in_seconds_after_epoch=$(date -d "$timestamp_of_attacker_exit" +%s)
+time_spent_attacking=$(($attacker_exit_in_seconds_after_epoch - $attacker_entry_in_seconds_after_epoch))
+file="/home/student/mitm_logs/$contname.log$fileend"
+total=0
+num_lines=0
+while IFS= read -r line; do
+    # Check if the line is a line where the attacker wrote commands
+    if echo "$line" | grep -q "line from reader"; then
+        num_chars=$(echo $line | cut -d':' -f4)
+        # one is subtracted because the lines start with a whitespace character
+        total=$(($total + ${#num_chars} - 1))
+        num_lines=$(($num_lines + 1))
+    fi
+done < "$file"
+average_length_of_commands=$(bc -l <<< "$total/$num_lines")
+# Information is added to the end of the respective counter file
+echo "$numcommands - $time_spent_attacking - $average_length_of_commands" >> /home/student/mitm_logs/"$1"counter
