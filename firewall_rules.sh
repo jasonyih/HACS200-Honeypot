edaceb8465a48f8a4f1fdbaf98867131be804e69
Jason Yih <jjyih2@gmail.com>
2023-10-18 18:19:34 -0400

    Uncommented commands at top of firewall.sh

--- firewall_rules.sh
+++ firewall_rules.sh
@@ -6,8 +6,8 @@
 #
 # To enable the firewall, you may need to enable the br_netfilter kernel module
 # by running the following commands:
-# modprobe br_netfilter
-# sysctl -p /etc/sysctl.conf
+modprobe br_netfilter
+sysctl -p /etc/sysctl.conf
 #
 
 #pve-firewall restart
