a5cef9f20b13221d88998ef32cf6fabf661c93fc
azapo <109777180+azapo@users.noreply.github.com>
2023-10-19 13:25:16 -0400

    FIxed issue with cron

--- container.sh
+++ container.sh
@@ -11,20 +11,22 @@ then
         exit 1
 fi
 
-contname=$1
+fourthipfield=$(echo "$2" | cut -d'.' -f4)
+customport=$(( fourthipfield + 50000 ))
 
+contname=$1
+sudo lxc-ls > /home/student/temp"$2"
 # checks to see if the container already exists. If so, it is stopped and destroyed. If not, it is created and run
-if sudo lxc-ls | grep -q "$1 "
+if grep -q "$1" /home/student/temp"$2"
 then
     # the lines below are used to destroy the container and remove all the firewall rules to do with it
-    ip=$(sudo lxc-ls -f | grep "$1 " | cut -d'-' -f2 | awk '{$1=$1};1')
+    ip=$(sudo lxc-ls -f | grep "$1" | cut -d'-' -f2 | awk '{$1=$1};1')
 
-    # MAKE SPECIFIC FOR CONTAINER/IP
-    sudo forever stop /home/student/MITM/mitm.js
-    sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$2" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:3355
-    sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$2" --jump DNAT --to-destination "$ip"
-    sudo iptables --table nat --delete POSTROUTING --source "$ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$2"
-    sudo ip addr delete "$2"/"$3" brd + dev eth1
+    sudo forever stop /home/student/MITM/mitm.js -n "$contname"
+    sudo /usr/sbin/iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$2" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:"$customport"
+    sudo /usr/sbin/iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$2" --jump DNAT --to-destination "$ip"
+    sudo /usr/sbin/iptables --table nat --delete POSTROUTING --source "$ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$2"
+    sudo /usr/sbin/ip addr delete "$2"/"$3" brd + dev eth1
     sudo lxc-stop -n "$1"
     sleep 5
     sudo lxc-destroy -n "$1"
@@ -35,13 +37,13 @@ else
     # install an OpenSSH server on it.
     sudo lxc-create -n "$1" -t download -- -d ubuntu -r focal -a amd64
     sudo lxc-start -n "$1"
-    sleep 5
+    sleep 60
     sudo ip addr add "$2"/"$3" brd + dev eth1
     sudo ip link set dev eth1 up
     ip=$(sudo lxc-ls -f | grep "$1 " | cut -d'-' -f2 | awk '{$1=$1};1')
-    sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$2" --jump DNAT --to-destination "$ip"
-    sudo iptables --table nat --insert POSTROUTING --source "$ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$2"
-    sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$2" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:3355
+    sudo /usr/sbin/iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$2" --jump DNAT --to-destination "$ip"
+    sudo /usr/sbin/iptables --table nat --insert POSTROUTING --source "$ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$2"
+    sudo /usr/sbin/iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$2" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:"$customport"
     sudo lxc-attach -n "$1" -- bash -c "sudo apt-get update && sudo apt-get install -y openssh-server"
 
     # this line create a honey directory named "confidential" on the container
@@ -53,7 +55,7 @@ else
         done
 
     # sets up forever
-    sudo sysctl -w net.ipv4.conf.all.route_localnet=1
+    sudo /usr/sbin/sysctl -w net.ipv4.conf.all.route_localnet=1
     sudo npm install -g forever
 
     # variable used to determine the last log for this container
@@ -66,5 +68,5 @@ else
     done
 
     # sets up the MITM server for this container
-    sudo forever -l /home/student/mitm_logs/"$1".log"$fileend" start /home/student/MITM/mitm.js -n "$1" -i "$ip" -p 3355 --auto-access --auto-access-fixed 2 --debug
+    sudo forever -l /home/student/mitm_logs/"$1".log"$fileend" start /home/student/MITM/mitm.js -n "$1" -i "$ip" -p "$customport" --auto-access --auto-access-fixed 2 --debug
 fi
